8f90c4bdb9455541e750a5bb4a404fb23698bdbc
nek_vasia <nek_vasia@mail.ru>
2023-10-13 20:18:16 +0300

    Добавил скрипт калькулятора

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+function calculate() {
+    var number1 = parseFloat(document.getElementById('num1').value);
+    var number2 = parseFloat(document.getElementById('num2').value);
+    var operation = document.getElementById('operation').value;
+    var result;
+
+    if (operation === 'calculator__option-addition') {
+        result = number1 + number2;
+    } else if (operation === 'calculator__option-subtraction') {
+        result = number1 - number2;
+    } else if (operation === 'calculator__option-multiplication') {
+        result = number1 * number2;
+    } else if (operation === 'calculator__option-division') {
+        result = number1 / number2;
+    }
+
+    document.getElementById('result').textContent = 'Результат: ' + result;
+}
